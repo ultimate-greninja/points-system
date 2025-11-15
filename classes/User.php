ca9d3a37495332dea56488fa2261544ba091375c
Thomas Bradshaw <thomasdavidbradshaw@gmail.com>
2025-11-15 13:35:12 +0000

    started making User class

---
+++ classes/User.php
@@ -0,0 +1,17 @@
+<?php
+
+class User {
+    private $id;
+    private $username;
+    private $class;
+    private $level;
+    private $exp;
+    private $gold;
+    private $hp;
+    private $attack;
+    private $equipedSkill1;
+    private $equipedSkill2;
+    private $equipedSkill3;
+    
+
+}
